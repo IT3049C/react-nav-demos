7ac8e82f9cfaa08f3c291891fe65d9c907e55018
Yahya Gilany <yahya.gilany@live.com>
2025-10-19 14:23:31 -0400

    routing on main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppLayout from "./AppLayout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Events } from "./pages/Events";
+import { EventDetails } from "./pages/EventDetails";
+import { Settings } from "./pages/Settings";
+import { NotFound } from "./pages/NotFound";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: `/`, element: <Home /> },
+      { path: `/about`, element: <About /> },
+      { path: `/events`, element: <Events /> },
+      { path: `/events/details`, element: <EventDetails /> },
+      { path: `/settings`, element: <Settings /> },
+      { path: `*`, element: <NotFound /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
