b0abbc1acaefed52a83709601cb417fac31b20cb
Yahya Gilany <yahya.gilany@live.com>
2025-11-23 17:33:48 -0500

    updated the base name for the browsers router

--- src/main.jsx
+++ src/main.jsx
@@ -12,27 +12,32 @@ import { LoginPage } from "./pages/LoginPage";
 import { RequireAuth } from "./auth/RequireAuth";
 import { AuthProvider } from "./auth/AuthProvider";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      children: [
+        { path: `/`, element: <Home /> },
+        { path: `/login`, element: <LoginPage /> },
+        { path: `/about`, element: <About /> },
+        { path: `/events`, element: <Events /> },
+        { path: `/events/:eventId`, element: <EventDetails /> },
+        {
+          path: `/settings`,
+          element: (
+            <RequireAuth>
+              <Settings />
+            </RequireAuth>
+          ),
+        },
+        { path: `*`, element: <NotFound /> },
+      ],
+    },
+  ],
   {
-    element: <AppLayout />,
-    children: [
-      { path: `/`, element: <Home /> },
-      { path: `/login`, element: <LoginPage /> },
-      { path: `/about`, element: <About /> },
-      { path: `/events`, element: <Events /> },
-      { path: `/events/:eventId`, element: <EventDetails /> },
-      {
-        path: `/settings`,
-        element: (
-          <RequireAuth>
-            <Settings />
-          </RequireAuth>
-        ),
-      },
-      { path: `*`, element: <NotFound /> },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
